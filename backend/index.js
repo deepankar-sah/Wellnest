5366e8ae369846b803797bde66310dbbb38f8d41
Deepankar Sah <deepankerrocking@gmail.com>
2025-05-26 23:19:03 +0530

    basic setup complete of backend

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Backend is working!');
+});
+
+app.get('/home', (req, res) => {
+  res.send ('homepage pe aa gye hain');
+})
+
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("MongoDB connected");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}).catch(err => console.error(err));
